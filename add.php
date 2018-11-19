e1478dbf53e17634020ef7ca0cad8e3c2e670c52
hannahher <45140643+hannahher@users.noreply.github.com>
2018-11-19 14:01:00 +0100

    Add files via upload

---
+++ add.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+<h1> Übung </h1> 
+
+<form action="index.php" method="get" >
+
+Name <br>
+<input type="text" name="name"/>
+<br>
+<br>
+
+<select name="Tageszeit">
+	<option value="Guten Morgen"> Morgens</option>
+ 	<option value="Mahlzeit">Mittags</option>
+	<option value="Guten Abend">Abends</option>
+	<option value="Gute Nacht">Nachts</option>
+</select>
+
+<h2> I say YES! to... </h2>
+
+<select name="Unfug0">
+	<option value="Unfug0"> Unfug 0 </option>
+</select> 
+
+<select name="Unfug1">
+	<option value="Unfug1"> Unfug 1 </option>
+</select>
+
+<select name="Unfug2">
+	<option value="Unfug2"> Unfug 2 </option>
+</select>
+
+<br>
+<br>
+	<input type= "submit" value="Abschicken"/>
+</form>
+</body>
+</html> 
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+
+<?php 
+echo $_GET['Tageszeit']; 
+echo ",  "   ; 
+echo $_GET['name']; 
+echo "!"  ;
+
+?>
+</body> 
+</html>
