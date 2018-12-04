a59b9648f0221e1efc6eec4b7fa8a62a29a45d95
hannahher <45140643+hannahher@users.noreply.github.com>
2018-12-04 17:33:30 +0100

    Eingaben werden gespeichert. Navigationsleiste + Footer.

--- add.php
+++ add.php
@@ -1,16 +1,21 @@
 <!DOCTYPE HTML>
 <html> 
 <body>
+<?php
+	include('nav.inc')
+?>
 <h1> Übung </h1> 
 
-<form action="index.php" method="get" >
+<form action="index.php" method="post" >
 
-Name <br>
-<input type="text" name="name"/>
+Name: <br>
+<input type="text" name="name"/> <br>
+E-Mail: <br> <input type="text" name="mail"/>
 <br>
 <br>
 
 <select name="Tageszeit">
+	<option> </option>
 	<option value="Guten Morgen"> Morgens</option>
  	<option value="Mahlzeit">Mittags</option>
 	<option value="Guten Abend">Abends</option>
@@ -19,21 +24,37 @@ Name <br>
 
 <h2> I say YES! to... </h2>
 
-<select name="Unfug0">
-	<option value="Unfug0"> Unfug 0 </option>
+<select name="Essen">
+	<option> </option>
+	<option value="Muffins"> Muffins </option>
+	<option value="Kuchen">Kuchen</option>
+	<option value="Brezel">Brezel</option>
+	<option value="Pommes">Pommes</option>
 </select> 
 
-<select name="Unfug1">
-	<option value="Unfug1"> Unfug 1 </option>
+<select name="Freizeitbeschäftigung">
+	<option> </option>
+	<option value="Konzert"> Konzert </option>
+	<option value="Weihnachtsmarkt">Weihnachtsmarkt</option>
+	<option value="Kino">Kino</option>
+	<option value="Netflix">Netflix</option>
 </select>
 
-<select name="Unfug2">
-	<option value="Unfug2"> Unfug 2 </option>
+<select name="Programm">
+	<option> </option>
+	<option value="HTML"> HTML </option>
+	<option value="Java">Java</option>
+	<option value="PHP">PHP</option>
+	<option value="Git">GIT</option>
 </select>
 
 <br>
 <br>
 	<input type= "submit" value="Abschicken"/>
 </form>
+<br><br><br><br><br><br>
+<?php
+include ('footer.inc');
+?>
 </body>
 </html> 
--- index.php
+++ index.php
@@ -1,13 +1,37 @@
+
+<?php
+include("config.ini");
+$text = urldecode($_REQUEST['Essen']) ."   " .urldecode($_REQUEST['Freizeitbeschäftigung']) ."   " .urldecode($_REQUEST['Programm']) ."\n";
+file_put_contents("test.txt", $text, FILE_APPEND);
+
+
+?>
 <!DOCTYPE HTML>
 <html> 
 <body>
 
-<?php 
-echo $_GET['Tageszeit']; 
-echo ",  "   ; 
-echo $_GET['name']; 
-echo "!"  ;
+<?php
+include('nav.inc');
+?>
 
+<?php 
+
+// Falls der Name nicht übertragen wurde... 
+	if (empty($_POST['name'])){
+	// anonyme Begrüßung
+	echo "Guten Tag!"; 
+	}
+// Falls ein Name übertragen wurde 
+	else {
+	echo $_POST['Tageszeit']; }
+?>
+
+<br><br><br><br><br><br>
+
+
+
+<?php
+include ('footer.inc');
 ?>
 </body> 
 </html>
