706ea35926d630b0d649619d53f5292707b7f8a5
hannahher <45140643+hannahher@users.noreply.github.com>
2019-01-06 14:37:32 +0100

    Fehlermeldung, wenn Name o. Email nicht eingegeben

--- add.php
+++ add.php
@@ -1,60 +1,61 @@
 <!DOCTYPE HTML>
-<html> 
-<body>
-<?php
-	include('nav.inc')
-?>
-<h1> Übung </h1> 
+	<html> 
+	<body>
+	<?php
+		include('nav.inc')
+	?>
+	<h1> Übung </h1> 
+	
+	
+	<form action="index.php" method="post" >
+	
+	Name: <br>
+	<input type="text" name="name"/> <br>
+	E-Mail: <br> <input type="text" name="mail"/>
+	<br>
+	<br>
 
-<form action="index.php" method="post" >
+	<select name="tageszeit">
+		<option> </option>
+		<option value="Guten Morgen"> Vormittags </option>
+		<option value="Mahlzeit"> Mittags </option>
+		<option value="Guten Abend"> Abends </option>
+	</select> 
 
-Name: <br>
-<input type="text" name="name"/> <br>
-E-Mail: <br> <input type="text" name="mail"/>
-<br>
-<br>
+<h1> I say Yes to.. </h1> 
 
-<select name="Tageszeit">
-	<option> </option>
-	<option value="Guten Morgen"> Morgens</option>
- 	<option value="Mahlzeit">Mittags</option>
-	<option value="Guten Abend">Abends</option>
-	<option value="Gute Nacht">Nachts</option>
-</select>
+	<select name="Freizeitbeschäftigung">
+		<option> </option>
+		<option value="Konzert"> Konzert </option>
+		<option value="Weihnachtsmarkt">Weihnachtsmarkt</option>
+		<option value="Kino">Kino</option>
+		<option value="Netflix">Netflix</option>
+	</select>
 
-<h2> I say YES! to... </h2>
+	<select name="Süßigkeiten">
+		<option> </option>
+		<option value="Kuchen"> Kuchen </option>
+		<option value="Schokolade">Schokolade</option>
+		<option value="Chips">Chips</option>
+		<option value="Gummibärchen">Gummibärchen</option>
+	</select>
+	
+	<select name="Programm">
+		<option> </option>
+		<option value="HTML"> HTML </option>
+		<option value="Java">Java</option>
+		<option value="PHP">PHP</option>
+		<option value="Git">GIT</option>
+	</select>
+	
+	<br>
+	<br>
+		<input type= "submit" value="Abschicken"/>
+	</form>
 
-<select name="Essen">
-	<option> </option>
-	<option value="Muffins"> Muffins </option>
-	<option value="Kuchen">Kuchen</option>
-	<option value="Brezel">Brezel</option>
-	<option value="Pommes">Pommes</option>
-</select> 
-
-<select name="Freizeitbeschäftigung">
-	<option> </option>
-	<option value="Konzert"> Konzert </option>
-	<option value="Weihnachtsmarkt">Weihnachtsmarkt</option>
-	<option value="Kino">Kino</option>
-	<option value="Netflix">Netflix</option>
-</select>
-
-<select name="Programm">
-	<option> </option>
-	<option value="HTML"> HTML </option>
-	<option value="Java">Java</option>
-	<option value="PHP">PHP</option>
-	<option value="Git">GIT</option>
-</select>
-
-<br>
-<br>
-	<input type= "submit" value="Abschicken"/>
-</form>
-<br><br><br><br><br><br>
-<?php
-include ('footer.inc');
-?>
-</body>
-</html> 
+	<br><br><br><br><br><br>
+	<?php
+	include ('footer.inc');
+	?>
+	</body>
+	</html>
--- index.php
+++ index.php
@@ -1,37 +1,46 @@
 
-<?php
-include("config.ini");
-$text = urldecode($_REQUEST['Essen']) ."   " .urldecode($_REQUEST['Freizeitbeschäftigung']) ."   " .urldecode($_REQUEST['Programm']) ."\n";
-file_put_contents("test.txt", $text, FILE_APPEND);
-
-
-?>
-<!DOCTYPE HTML>
-<html> 
-<body>
-
-<?php
-include('nav.inc');
-?>
-
-<?php 
-
-// Falls der Name nicht übertragen wurde... 
+	<?php
+	include("config.inc");
+	$text = urldecode($_REQUEST['Süßigkeiten']) ."   " .urldecode($_REQUEST['Freizeitbeschäftigung']) ."   " .urldecode($_REQUEST['Programm']) ."\n";
+	file_put_contents("test.txt", $text, FILE_APPEND);
+	
+	
+	?>
+	<!DOCTYPE HTML>
+	<html> 
+	<body>
+	
+	<?php
+	include('nav.inc');
+	?>
+	
+	<?php 
+	
 	if (empty($_POST['name'])){
-	// anonyme Begrüßung
-	echo "Guten Tag!"; 
+		echo $_POST['tageszeit'] ."! <br>" ;
 	}
-// Falls ein Name übertragen wurde 
 	else {
-	echo $_POST['Tageszeit']; }
-?>
+		echo $_POST['tageszeit'] .", " .$_POST['name'] ."! <br>"; 
+	}
+	// Falls der Name nicht übertragen wurde... 
+		if (empty($_POST['mail'])){
+		// anonyme Begrüßung
+		echo "Es wurde leider keine E-Mail angegeben!"; 
+		}
+	// Falls ein Name übertragen wurde 
+		else {
+		 echo "Vielen Dank für ihre Mailadresse."; 
+		}
 
-<br><br><br><br><br><br>
-
-
-
-<?php
-include ('footer.inc');
-?>
-</body> 
-</html>
+	echo " <br>" ."I say Yes to..." ." " .$text
+	?>
+	
+	<br><br><br><br><br><br>
+	
+	
+	
+	<?php
+	include ('footer.inc');
+	?>
+	</body> 
+	</html>
